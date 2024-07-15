840f5cb595f68b6896eb6a6e8ff39c1d7e9c4026
Nourchene Hamrita <78702994+Nourchene-Hamrita@users.noreply.github.com>
2024-07-15 23:33:13 +0100

    Add attributes created_at/updated_at

--- sel-demande-api/src/main/java/com/RNE/demande/entities/Adresse.java
+++ sel-demande-api/src/main/java/com/RNE/demande/entities/Adresse.java
@@ -1,14 +1,8 @@
 package com.RNE.demande.entities;
 
 import java.io.Serializable;
-import java.util.Date;
-import java.util.List;
-import java.util.Set;
-
+import java.time.LocalDateTime;
 import com.RNE.demande.enums.AdresseType;
-import com.RNE.demande.enums.Genre;
-import com.RNE.demande.enums.TypeIdCarte;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -18,55 +12,66 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Adresse implements Serializable{
+public class Adresse implements Serializable {
 
-	 /**
-		 * 
-		 */
-		private static final long serialVersionUID = -6047593005293832737L;
-		/**
-		* 
-		*/
+    private static final long serialVersionUID = -6047593005293832737L;
 
-		@Id
-	    @GeneratedValue(strategy = GenerationType.IDENTITY)
-	    private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	    @Column(name = "gov_code")
-	    private String govCode;
+    @Column(name = "gov_code")
+    private String govCode;
 
-	    @Column(name = "code_Ville")
-	    private String codeVille;
+    @Column(name = "code_Ville")
+    private String codeVille;
 
-	    @Column(name = "code_Postal")
-	    private String codePostal;
-	    
-	    @Column(name = "rue_fr")
-	    private String rueFr;
-	    
-	    
-	    @Column(name = "rue_Ar")
-	    private String rueAr;
-	    
-	    @ManyToOne
-	    private Societe societe;
-	    
-	    @OneToOne(mappedBy = "adresse")
-	    private Personne personne;
-	    
+    @Column(name = "code_Postal")
+    private String codePostal;
 
-		@Enumerated(EnumType.STRING)
-		@Column(name = "adresse_type")
-		private AdresseType adresseType;
+    @Column(name = "rue_fr")
+    private String rueFr;
 
+    @Column(name = "rue_Ar")
+    private String rueAr;
 
+    @ManyToOne
+    private Societe societe;
+
+    @OneToOne(mappedBy = "adresse")
+    private Personne personne;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "adresse_type")
+    private AdresseType adresseType;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        LocalDateTime now = LocalDateTime.now();
+        createdAt = now;
+        updatedAt = now;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
 }
--- sel-demande-api/src/main/java/com/RNE/demande/entities/MembreSpec.java
+++ sel-demande-api/src/main/java/com/RNE/demande/entities/MembreSpec.java
@@ -1,6 +1,7 @@
 package com.RNE.demande.entities;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 
@@ -8,6 +9,7 @@ import com.RNE.demande.enums.Genre;
 import com.RNE.demande.enums.Pouvoirs;
 import com.RNE.demande.enums.Qualite;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -16,7 +18,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
@@ -30,46 +33,58 @@ import lombok.Setter;
 @Getter
 @Setter
 public class MembreSpec implements Serializable {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 6974072593311623828L;
+    private static final long serialVersionUID = 6974072593311623828L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@Enumerated(EnumType.STRING)
-	private Qualite qualite;
+    @Enumerated(EnumType.STRING)
+    private Qualite qualite;
 
-	@Enumerated(EnumType.STRING)
-	private Pouvoirs pouvoirs;
+    @Enumerated(EnumType.STRING)
+    private Pouvoirs pouvoirs;
 
-	@Temporal(TemporalType.DATE)
-	private Date dateNomination;
+    @Temporal(TemporalType.DATE)
+    private Date dateNomination;
 
-	@Temporal(TemporalType.DATE)
-	private Date dateFinNomination;
+    @Temporal(TemporalType.DATE)
+    private Date dateFinNomination;
 
-	private Long dureeNomination;
-	
-	private Boolean isDeposant;
-	
-	@Enumerated(EnumType.STRING)
-	private Genre genre; 
-	
-	private String email;
-	
-	private int numero;
-	
-	private String adresse;
+    private Long dureeNomination;
+    
+    private Boolean isDeposant;
+    
+    @Enumerated(EnumType.STRING)
+    private Genre genre; 
+    
+    private String email;
+    
+    private int numero;
+    
+    private String adresse;
 
-	@ManyToOne
-	private Societe societe;
-	
+    @ManyToOne
+    private Societe societe;
+    
+    @ManyToMany
+    private List<Personne> personnes;
 
-	@ManyToMany
-	private List<Personne> personnes;
-	
-	
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        LocalDateTime now = LocalDateTime.now();
+        createdAt = now;
+        updatedAt = now;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
 }
