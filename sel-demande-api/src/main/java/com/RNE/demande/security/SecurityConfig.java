fbb5e8896e04b7203e204a3b3efe19589314d2d1
Nourchene Hamrita <78702994+Nourchene-Hamrita@users.noreply.github.com>
2024-08-28 21:52:19 +0100

    docker sel-demande-api

--- sel-demande-api/src/main/java/com/RNE/demande/security/SecurityConfig.java
+++ sel-demande-api/src/main/java/com/RNE/demande/security/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.RNE.demande.security;
 
-import java.util.Arrays;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -9,43 +8,40 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity
 public class SecurityConfig {
-	
-	 private JwtAuthConverter jwtAuthConverter;
 
-	 public SecurityConfig(JwtAuthConverter jwtAuthConverter) {
-	        this.jwtAuthConverter = jwtAuthConverter;
-	    }
-	 
-	  @Bean
-	    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-	        return httpSecurity
-	                .cors(Customizer.withDefaults())
-	                .sessionManagement(sm->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-	                .csrf(csrf->csrf.disable())
-	                .headers(h->h.frameOptions(fo->fo.disable()))
-	                .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
-	                .oauth2ResourceServer(o2->o2.jwt(jwt->jwt.jwtAuthenticationConverter(jwtAuthConverter)))
-	                .build();
-	    }
-	 @Bean
-	    CorsConfigurationSource corsConfigurationSource() {
-	        CorsConfiguration configuration = new CorsConfiguration();
-	        configuration.setAllowedOrigins(Arrays.asList("*"));
-	        configuration.setAllowedMethods(Arrays.asList("*"));
-	        configuration.setAllowedHeaders(Arrays.asList("*"));
-	        configuration.setExposedHeaders(Arrays.asList("*"));
-	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	        source.registerCorsConfiguration("/**", configuration);
-	        return source;
-	    }
-	
+    private JwtAuthConverter jwtAuthConverter;
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    private String jwkSetUri;
+
+    public SecurityConfig(JwtAuthConverter jwtAuthConverter) {
+        this.jwtAuthConverter = jwtAuthConverter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .cors(Customizer.withDefaults())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(csrf -> csrf.disable())
+                .headers(h -> h.frameOptions(fo -> fo.disable()))
+                .authorizeHttpRequests(ar -> ar.requestMatchers("/api/demande/**").permitAll())
+                .authorizeHttpRequests(ar -> ar.anyRequest().authenticated())
+                .oauth2ResourceServer(o2 -> o2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter)))
+                .build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
+    }
 }
